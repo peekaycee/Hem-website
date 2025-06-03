865bd50fec1b168a72706eaa156a337ccdf64160
peekaycee <kalukaluokomba@gmail.com>
2025-06-04 00:51:51 +0100

    rearrange files

--- web-app/src/app/follow-up/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function FollowUp() {
-  return (
-    <section>
-      <h1>Follow up and Hospitality</h1>
-      {/* church follow up activities */}
-    </section>
-  );
-}
--- web-app/src/app/ministers-and-workers/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function MinistersAndWorkers() {
-  return (
-    <section>
-      <h1>Ministers & Workers Info</h1>
-      {/* Sacrament Details + Ministry Info */}
-    </section>
-  );
-}
